48742d1ce270a0efc60518a1fb6426e00c1ce54e
Nikita <sheihnt@gmail.com>
2024-12-18 06:03:09 +0300

    Create Select.sql

---
+++ Coursework/6.Tests Query/Select.sql
@@ -0,0 +1,16 @@
+--Для таблицы Services:
+SELECT * FROM Services;
+--Для таблицы Tariffs:
+SELECT * FROM Tariffs;
+--Для таблицы Customers:
+SELECT * FROM Customers;
+--Для таблицы Executor:
+SELECT * FROM Executor;
+--Для таблицы InProcess:
+SELECT * FROM InProcess;
+--Для таблицы Result:
+SELECT * FROM Result;
+--Для таблицы Payment:
+SELECT * FROM Payment;
+--Для таблицы AllResult:
+SELECT * FROM AllResult;
